9789b527738762a9ecf0ae8e2510860945496994
Juhi1919 <juhipatel9394@gmail.com>
2018-08-18 23:05:50 -0400

    add

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,11 @@
-DROP DATABASE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS `burgers`;
+CREATE DATABASE `burgers`;
 
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
+USE burgers;
 
 CREATE TABLE burgers (
-id INT NOT NULL AUTO_INCREMENT,
-burger_name VARCHAR(250) NOT NULL,
-devoured BOOLEAN DEFAULT false,
-date TIMESTAMP,
-PRIMARY KEY (id)
+id int AUTO_INCREMENT,
+PRIMARY KEY (id),
+burger_name varchar(50) NOT NULL,
+devoured BOOLEAN default false
 );
-
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,7 @@
-INSERT INTO burgers_db.burgers(burger_name) 
-VALUES ("Cheeseburger"), ("Pizza Burger"), ("Steakburger");
--- remember that true shows up as 1 and false shows up as 0 in the table.
+USE burgers;
 
-SELECT * FROM burgers;
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Vegetable Burger");
+
+select * from burgers;
